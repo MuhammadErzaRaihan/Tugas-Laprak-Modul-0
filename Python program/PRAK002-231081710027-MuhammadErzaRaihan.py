59279c0cf47f907ef0142e7826ecb510d93d0d87
MuhammadErzaRaihan <145301371+MuhammadErzaRaihan@users.noreply.github.com>
2023-09-19 10:39:15 +0800

    Add files via upload

---
+++ Python program/PRAK002-231081710027-MuhammadErzaRaihan.py
@@ -0,0 +1,3 @@
+print("Selamat Pagi, Erza")
+print("Selamat Siang, Erza")
+print("Selamat Malam, Erza")
---
+++ Python program/PRAK004-2310817210027-MuhammadErzaRaihan.py
@@ -0,0 +1,9 @@
+nama = "Muhammad Erza Raihan"
+nim= "2310817210027"
+
+print("##############################")
+print("#                            #")
+print(f"#  {nama}      #")
+print(f"#      {nim}         #")
+print("#                            #")
+print("##############################")
